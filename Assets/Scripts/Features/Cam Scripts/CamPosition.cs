f5d688fbf7b6ae2df7b8d5609d2484290ddf3a40
Lukas Prinz <prinz.lukas99@gmail.com>
2022-01-30 15:01:24 +0100

    Camera System Upgrade A bug in the first time zoom in and out

--- Assets/Scripts/Features/Cam Scripts/CamPosition.cs
+++ Assets/Scripts/Features/Cam Scripts/CamPosition.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CamPosition : MonoBehaviour
 {
     [SerializeField] private Transform camPoints;
+    [SerializeField] MovePoints movePoints;
     public Transform currentPoint { get; set; }
 
     private float interpolation;
@@ -13,13 +14,24 @@ public class CamPosition : MonoBehaviour
 
     }
 
-    private void Update()  
+    private void Update()
     {
         interpolation = Time.deltaTime * 0.5f;
         transform.position = Vector3.Lerp(transform.position, currentPoint.position, interpolation);
+
+        if (Input.GetKey(KeyCode.Q) && transform.position.y < 18.78f)
+        {
+            transform.position += new Vector3(0, movePoints.CamSpeed, -movePoints.CamSpeed);
+        }
+        else if (Input.GetKey(KeyCode.E) && transform.position.y > 11.7f)
+        {
+            transform.position -= new Vector3(0, movePoints.CamSpeed, -movePoints.CamSpeed);
+        }
+
+        //Vector3.ClampMagnitude(transform.position, movePoints.MaxZoomOut);
     }
 
-   
+
 
 
 
--- Assets/Scripts/Features/Cam Scripts/MovePoints.cs
+++ Assets/Scripts/Features/Cam Scripts/MovePoints.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class MovePoints : MonoBehaviour
 {
+    [SerializeField] private float cameraSpeed;
+    public float CamSpeed => cameraSpeed;
+
+    [SerializeField] private float maxZoomIn;
+    public float MaxZoomIn => maxZoomIn;
+
+    [SerializeField] private float maxZoomOut;
+    public float MaxZoomOut => maxZoomOut;
+
+
     private void Update()
     {
         MoveCamera();
@@ -9,13 +19,13 @@ public class MovePoints : MonoBehaviour
 
     public void MoveCamera()
     {
+
         if (Input.GetKey(KeyCode.Q))
-        {
-            transform.position += new Vector3(0, 0.04f, 0);
-        }
-        else if (Input.GetKey(KeyCode.E))
-        {
-            transform.position -= new Vector3(0, 0.04f, 0);
-        }
+            transform.position += new Vector3(0, cameraSpeed, -cameraSpeed);
+        else if(Input.GetKey(KeyCode.E))
+            transform.position -= new Vector3(0, cameraSpeed, -cameraSpeed);
+
+        transform.position = Vector3.ClampMagnitude(transform.position, MaxZoomOut);
+        transform.position = Vector3.ClampMagnitude(transform.position, MaxZoomIn);
     }
 }
