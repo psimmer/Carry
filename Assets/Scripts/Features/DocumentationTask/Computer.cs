93453becf89ef4d537f84246762dafb543073df3
Patrick Simmer <simmer.patrick@gmail.com>
2022-02-08 16:44:51 +0100

    working on save/load system computer

--- Assets/Scripts/Features/DocumentationTask/Computer.cs
+++ Assets/Scripts/Features/DocumentationTask/Computer.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
+[System.Serializable]
 public class Computer : MonoBehaviour, ISaveSystem
 {
     [SerializeField] private GameObject clipBoardCanvas;
@@ -20,6 +23,8 @@ public class Computer : MonoBehaviour, ISaveSystem
     {
         canvas.gameObject.SetActive(false);
         ClipBoardCanvas.gameObject.SetActive(false);
+        if (GlobalData.instance.IsSaveFileLoaded)
+            LoadData();
     }
 
     private void Update()
@@ -34,13 +39,53 @@ public class Computer : MonoBehaviour, ISaveSystem
         ClipBoardCanvas.gameObject.SetActive(true);
     }
 
-    public void LoadData()
-    {
-        //throw new System.NotImplementedException();
-    }
-
     public void SaveData()
     {
-        //throw new System.NotImplementedException();
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        string path = Application.persistentDataPath + "/SaveDataComputer.carry";
+        Debug.Log("Save File location: " + path);
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        //float[] camPosition = new float[6];
+        //formatter.Serialize(stream,documentationCamPos.position.x);
+        //formatter.Serialize(stream,documentationCamPos.position.y);
+        //formatter.Serialize(stream,documentationCamPos.position.z);
+        //camPosition[3] = documentationCamPos.rotation.x;
+        //camPosition[4] = documentationCamPos.position.x;
+        //camPosition[5] = documentationCamPos.position.x;
+        formatter.Serialize(stream, inputField.ToString());
+
+        stream.Close();
     }
+
+    public void LoadData()
+    {
+        string path = Application.persistentDataPath + "/SaveDataComputer.carry";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            Debug.Log("Save File loaded: " + path);
+
+            //float[] camPositino = new float[3];
+            //Vector3 vector = new Vector3();
+            
+            //vector.x = (float)formatter.Deserialize(stream);
+            //vector.y = (float)formatter.Deserialize(stream);
+            //vector.z = (float)formatter.Deserialize(stream);
+            Camera.main.transform.position = documentationCamPos.position;
+
+            inputField.text = (string)formatter.Deserialize(stream);
+
+            stream.Close();
+
+        }
+        else
+        {
+            Debug.Log("Save File not found" + path);
+        }
+    }
+
+    
 }
